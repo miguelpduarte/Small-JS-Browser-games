0b4b460d3f2ab71ab9f6c372544069a7b68a129d
Meiogordo <gamer-go@hotmail.com>
2017-09-15 20:50:46 +0100

    Am going to be starting tetris next! :D

---
+++ tetris.js
@@ -0,0 +1,5 @@
+//color is an object with the 3 color components for the piece (r,g,b)
+//positions are the array of position objects, relative to the center, for each piece
+function TPiece(color = {r, g, b}, positions){
+	
+}
